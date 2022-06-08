47a3a24abb31821c7ae3aac54eef330740bc5908
ThomasBrossier <98547958+ThomasBrossier@users.noreply.github.com>
2022-06-08 13:23:41 +0200

    add event page & link between each page

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,10 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: path.resolve(__dirname, "src/index.js"),
+  entry: {
+      main : path.resolve(__dirname, "src/index.js"),
+      event : path.join(__dirname, "src/event.js"),
+  },
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "[name].bundle.js"
@@ -20,8 +23,15 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "src/index.html")
-    })
+      filename:'index.html', 
+      template: path.resolve(__dirname, "src/index.html"),
+      chunks : ["main"]
+    }),
+    new HtmlWebpackPlugin({
+        filename:'event.html', 
+        template: path.resolve(__dirname, "src/event.html"),
+        chunks : ["event"]
+      })
   ],
   devtool: "source-map",
   mode: "development",
